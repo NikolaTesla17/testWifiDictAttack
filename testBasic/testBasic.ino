e8a2c703ab54731fa6dfb058fb1627814a97cb53
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2020-06-03 21:17:13 -0700

    compilesNow

--- testBasic/testBasic.ino
+++ testBasic/testBasic.ino
@@ -1,31 +1,61 @@
 #include <ESP8266WiFi.h>        // Include the Wi-Fi library
 #include <ESP8266WiFiMulti.h>   // Include the Wi-Fi-Multi library
 
-const char* ssid = "ssid"; //Enter SSID
+const char* ssid = "KNAM"; //Enter SSID
 const char* password = "password"; //Enter Password
+int itNum = 1;
+char strValue[10];
+int ind = 0;
 
 void setup(void)
 { 
   Serial.begin(115200);
   // Connect to WiFi
-  WiFi.begin(ssid, password);
+  //WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) 
   {
-     delay(500);
-     Serial.print("*");
+    WiFi.begin(ssid, password); //eventually, you will have to add an identifier, for example, in serial event, check if ch is # and if so, assume it is the ssid
+    
+    Serial.print("trying password number ");
+    Serial.print(itNum);
+    Serial.print(": ");
+    Serial.print(password);
+    Serial.print('\n');
+    delay(50);
+    itNum++;
   }
-  
-  Serial.println("");
+  Serial.print('\n');
   Serial.println("WiFi connection Successful");
+  Serial.print('\n');
   Serial.print("The IP Address of ESP8266 Module is: ");
   Serial.print(WiFi.localIP());// Print the IP address
+  Serial.print('\n');
   Serial.print("The WiFi password for ");
   Serial.print(ssid);
   Serial.print(" is ");
   Serial.print(password);
 }
 
+
 void loop() 
 {
   // EMPTY
+}
+
+void serialEvent()
+{
+  while(Serial.available())
+  {
+    char ch = Serial.read();
+    Serial.write(ch);
+    if(ch != ',') { 
+            strValue[ind++] = ch; 
+      } else if(ch == ','){ 
+            strValue[ind] = 0; 
+            password = (strValue); 
+      }
+      strValue;
+      ind = 0;
+    
+  }
 }
